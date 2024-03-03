6544cd40ea789c04035ea2d61b4e950762abcfbf
Daniel Bulant <danbulant@gmail.com>
2024-03-03 18:34:46 +0100

    add controls for presenter

--- buffer-overflow-libc/src/project.ts
+++ buffer-overflow-libc/src/project.ts
@@ -9,3 +9,25 @@ import script from './scenes/script?scene';
 export default makeProject({
   scenes: [intro, program, code, printf, script],
 });
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "PageDown") {
+    // send fake space key
+    const event = new KeyboardEvent("keydown", {
+      key: " ",
+      code: "Space",
+      keyCode: 32,
+      which: 32
+    });
+    document.dispatchEvent(event);
+  } else if (e.key === "PageUp") {
+    // send fake arrow left key
+    const event = new KeyboardEvent("keydown", {
+      key: "ArrowLeft",
+      code: "ArrowLeft",
+      keyCode: 37,
+      which: 37
+    });
+    document.dispatchEvent(event);
+  }
+})
